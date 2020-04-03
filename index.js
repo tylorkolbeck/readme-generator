64732bb05ab5ead66a845431534d6c8f53417590
tylor.kolbeck@gmail.com <tylor.kolbeck@gmail.com>
2020-04-02 20:35:58 -0700

    change to async await

--- index.js
+++ index.js
@@ -1,22 +1,35 @@
 const fs = require("fs")
 const path = require("path")
+require("dotenv").config()
 const util = require("util")
-
-const writeFile = util.promisify(fs.writeFile)
-
+const moment = require("moment")
 const api = require("./utils/api")
 const generateMarkdown = require("./utils/generateMarkdown")
 const inquire = require("./inquire.js")
 
-function init() {
-  inquire()
-    .then(answers => generateMarkdown(answers))
-    .then(([filename, markdown]) => writeFile(`${filename}.md`, markdown))
-    .catch(error =>
-      error.isTtyError
-        ? console.log("Could not render in current environment.")
-        : console.log("Something went wrong", error)
-    )
+const writeFile = util.promisify(fs.writeFile)
+
+async function init() {
+  let answers = await inquire()
+
+  let { data } = await api.getUser(answers.username)
+
+  let markdown = generateMarkdown({
+    ...answers,
+    email: data.email,
+    avatar: data.avatar_url
+  })
+
+  await writeFile(
+    `${
+      answers.title
+        ? answers.title
+        : `${moment().format("MM-DD-YYYY-h:mm:ss")}-markdown`
+    }.md`,
+    markdown
+  )
+    .then(() => console.log("write complete"))
+    .catch(err => console.log("Error", err))
 }
 
 init()
