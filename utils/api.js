ec4e3374f44af94f18ef0838562c8128c4837f13
tylor.kolbeck@gmail.com <tylor.kolbeck@gmail.com>
2020-04-02 18:01:38 -0700

    add github api function call

--- utils/api.js
+++ utils/api.js
@@ -1,10 +1,25 @@
 const axios = require("axios");
-const dotenv = require("dotenv");
-
+require("dotenv").config();
+console.log(process.env.USER_TOKEN);
 const api = {
+  queryString: "https://api.github.com/users/",
   // TODO: Return promise for GitHub api response to get user data.
   // (Hint: Use axios to send a get request and return the promise created by calling axios.get())
-  getUser(username) {}
+  getUser(username) {
+    let apiCall = axios.get(`${this.queryString}${username}`, {
+      headers: {
+        authorization: `Bearer ${process.env.USER_TOKEN}`
+      }
+    });
+
+    return apiCall;
+  }
 };
 
+// api.getUser("tylorkolbeck").then(res => console.log(res));
+
 module.exports = api;
+
+// status
+// data.avatar_url
+// data.email
