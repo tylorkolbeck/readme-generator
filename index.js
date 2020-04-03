ac09db2fe317bc76190246150bf763b7a6399125
tylor.kolbeck@gmail.com <tylor.kolbeck@gmail.com>
2020-04-02 19:16:22 -0700

    remove commented out code

--- index.js
+++ index.js
@@ -8,19 +8,7 @@ const api = require("./utils/api")
 const generateMarkdown = require("./utils/generateMarkdown")
 const inquire = require("./inquire.js")
 
-// TODO: Write function to synchronously write data in the
-// current working directory to file named for the fileName parameter.
-// The data parameter is the text to write to the file.
-
-// TODO: Use inquirer to prompt the user for each question in the
-// questions array. Then call api.getUser to fetch the user profile
-// data from GitHub. Finally generate the markdown and use writeToFile
-// to create the README.md file.
 function init() {
-  // inquire()
-  // .then(answers => {
-  //   return answers
-  // })
   inquire()
     .then(answers => generateMarkdown(answers))
     .then(([filename, markdown]) => writeFile(`${filename}.md`, markdown))
