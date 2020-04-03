85d45ca299715d0d1e225aa90f45eabe9889d43b
tylor.kolbeck@gmail.com <tylor.kolbeck@gmail.com>
2020-04-02 18:05:52 -0700

    refactor headers

--- utils/api.js
+++ utils/api.js
@@ -1,22 +1,22 @@
 const axios = require("axios");
 require("dotenv").config();
-console.log(process.env.USER_TOKEN);
+
 const api = {
   queryString: "https://api.github.com/users/",
-  // TODO: Return promise for GitHub api response to get user data.
-  // (Hint: Use axios to send a get request and return the promise created by calling axios.get())
+  headers: {
+    authorization: `Bearer ${process.env.USER_TOKEN}`
+  },
+
   getUser(username) {
     let apiCall = axios.get(`${this.queryString}${username}`, {
-      headers: {
-        authorization: `Bearer ${process.env.USER_TOKEN}`
-      }
+      headers: this.headers
     });
 
     return apiCall;
   }
 };
 
-// api.getUser("tylorkolbeck").then(res => console.log(res));
+api.getUser("tylorkolbeck").then(res => console.log(res));
 
 module.exports = api;
 
