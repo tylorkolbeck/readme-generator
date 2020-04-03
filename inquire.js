37f3ac1cb38c15f085fb580616b00cf170314b4e
tylor.kolbeck@gmail.com <tylor.kolbeck@gmail.com>
2020-04-03 16:53:46 -0700

    update questions

--- inquire.js
+++ inquire.js
@@ -1,57 +1,57 @@
-const inquirer = require("inquirer");
+const inquirer = require("inquirer")
 
 const inquire = () => {
   return inquirer.prompt([
     {
       type: "input",
       name: "username",
-      message: "Github user?"
+      message: "Github user?",
     },
     {
       type: "input",
       name: "title",
-      message: "Project Title?"
+      message: "Project Title?",
     },
     {
       type: "input",
       name: "description",
-      message: "Project Description?"
+      message: "Project Description?",
     },
     {
       type: "input",
       name: "install",
       message: "Install Command?",
-      default: "npm install"
+      default: "npm install",
     },
     {
       type: "input",
       name: "usage",
       message: "Usage?",
-      default: "npm start"
+      default: "npm start",
     },
     {
       type: "list",
       name: "license",
       message: "License",
-      choices: ["MIT", "APACHE 2.0", "GPL 3.0", "BSD 3", "None"]
+      choices: ["MIT", "APACHE-2.0", "GPL-3.0", "BSD-3", "None"],
     },
     {
       type: "input",
-      name: "Contributing",
-      message: "Contributers?"
+      name: "contributers",
+      message: "Contributers (seperate by contributers by a ;)?",
     },
     {
       type: "input",
       name: "tests",
       message: "Tests Command?",
-      default: "npm test"
+      default: "npm test",
     },
     {
       type: "input",
-      name: "What does your user need to know to use the repo?",
-      message: ""
-    }
-  ]);
-};
+      name: "deployment",
+      message: "What does your user need to know to use the repo?",
+    },
+  ])
+}
 
-module.exports = inquire;
+module.exports = inquire
