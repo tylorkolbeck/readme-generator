abda13a1914c421f9c78a2424bbb868cb9fa983d
tylor.kolbeck@gmail.com <tylor.kolbeck@gmail.com>
2020-04-03 16:56:01 -0700

    update markdown format

--- utils/generateMarkdown.js
+++ utils/generateMarkdown.js
@@ -1,11 +1,68 @@
 // TODO: Return markdown string for README file given a data object.
 function generateMarkdown(data) {
-  let markdownString = `
-# ${data.title}
+  console.log(data)
+  return `
+  ## Project Name
+  ${data.title} 
+  
+  ## Owner 
+  
+  [![License](https://img.shields.io/static/v1?label=License&message=${
+    data.license
+  }&color=blue)](https://opensource.org/licenses/Apache-2.0)
 
+  ![GitHub Logo](${data.avatar}) 
+
+  Name: ${data.username}
+
+  Email: ${data.email ? data.email : "N/A"}
+
+  ## Description
+  ${data.description}
+  
+  ## Getting Started
+
+  To start the app.
+
+  \`\`\`
+  ${data.usage}
+  \`\`\`
+  
+  To install dependencies.
+  
+  \`\`\`
+  ${data.install}
+  \`\`\`
+  
+  ## Running the tests
+  
+  \`\`\`
+  ${data.tests}
+  \`\`\`
+
+  ## Deployment
+  
+  ${data.deployment}
+
+  ## Contributers
+  ${data.contributers
+    .split(";")
+    .map((cont) => {
+      return `- ${cont}\n`
+    })
+    .join("")
+    .trim()}
+  
+  ## License
+  
+  This project is licensed under the ${data.license} License
+  
+  ## Acknowledgments
+  
+  * Hat tip to anyone whose code was used
+  * Inspiration
+  * etc
 `
-
-  return [data.title, markdownString]
 }
 
 module.exports = generateMarkdown
